2dd3046ab26c896df4eecf635f7e46d91c50ae0f
Venkatesh Thimmaiah <venkatesh131780@gmail.com>
2023-08-28 13:38:54 +0530

    added login form and connected with firebase

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -3,10 +3,13 @@ import { useRef, useState } from "react";
 
 const SignUp = () => {
   const [isLoading, setIsLoading] = useState(false);
+  const [isLogin, setIsLogin] = useState(false);
+  const [formTitle, setFormTitle] = useState("Sign Up");
   const emailInputRef = useRef("");
   const passwordInputRef = useRef("");
 
-  const signUpHandler = () => {
+  const submitHandler = (e) => {
+    e.preventDefault();
     setIsLoading(true);
 
     const email = emailInputRef.current.value;
@@ -15,16 +18,69 @@ const SignUp = () => {
     const creds = {
       email: email,
       password: password,
+      returnSecureToken: true,
     };
 
     if (creds.email === "" || creds.password === "") {
       alert("Invalid creds");
     }
-    setIsLoading(false);
+    console.log(creds);
+    let url = "";
+    if (isLogin) {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAQHGDwuaaFsjKMS1h8GbOCG3sJQqPAcVY";
+      console.log("login working");
+    } else {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAQHGDwuaaFsjKMS1h8GbOCG3sJQqPAcVY";
+      console.log("signup working");
+    }
+    setIsLoading(true);
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify(creds),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        setIsLoading(false);
+        if (res.ok) {
+          return res.json();
+        } else {
+          return res.json().then((data) => {
+            let errorMessage = "Authentication Failed!";
+
+            throw new Error(errorMessage);
+          });
+        }
+      })
+      .then((data) => {
+        setIsLoading(false);
+        console.log(data);
+      })
+      .catch((error) => {
+        alert(error.messsage);
+      });
+    emailInputRef.current.value = "";
+    passwordInputRef.current.value = "";
   };
+
+  const enableLoginHander = (e) => {
+    e.preventDefault();
+    setFormTitle("Login");
+    setIsLogin(true);
+  };
+  const enableSignupHandler = (e) => {
+    e.preventDefault();
+    setFormTitle("Sign Up");
+    setIsLogin(false);
+  };
+
   return (
     <div className={classes.signup_form}>
-      <form>
+      <h2>{formTitle}</h2>
+      <form onSubmit={submitHandler}>
         <div className={classes.form_input}>
           <label htmlFor="email">Your Email</label>
           <input type="email" id="email" ref={emailInputRef} />
@@ -33,11 +89,23 @@ const SignUp = () => {
           <label htmlFor="password">Your Password</label>
           <input type="text" id="password" ref={passwordInputRef} />
         </div>
-        {isLoading ? (
-          <p>Sending request.....</p>
-        ) : (
-          <button onClick={signUpHandler}>Sign up</button>
-        )}
+        <div className={classes.buttons_container}>
+          {!isLoading && !isLogin && (
+            <div>
+              <button>Create Account</button>
+              <button onClick={enableLoginHander}>
+                Login With Exiting Account
+              </button>
+            </div>
+          )}
+          {!isLoading && isLogin && (
+            <div>
+              <button>Login</button>
+              <button onClick={enableSignupHandler}>create New Account</button>
+            </div>
+          )}
+        </div>
+        {isLoading && <p>Sending Request....</p>}
       </form>
     </div>
   );
